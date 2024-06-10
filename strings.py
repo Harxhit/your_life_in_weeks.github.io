6f2b7002bebe8fdf2c470952afdbef01309a375a
Harshit Singh <86526868+Harxhit@users.noreply.github.com>
2024-06-10 11:55:51 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -4,4 +4,4 @@ a = int(age)
 b = int(max_age)
 remaining_age = b - a
 weeks_left = remaining_age * 52
-print(f"You have {weeks_left} weeks left")
+print(f"You have {weeks_left} weeks left in your life")
