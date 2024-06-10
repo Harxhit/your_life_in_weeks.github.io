a17ae10a38a171761e9e6eeb79381bf3c03ef5e4
Harshit Singh <86526868+Harxhit@users.noreply.github.com>
2024-06-10 11:53:00 +0530

    Add files via upload
    Your life in weeks

---
+++ strings.py
@@ -0,0 +1,9 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+max_age = 90
+a = int(age)
+b = int(max_age)
+remaining_age = b - a
+weeks_left = remaining_age * 52
+print(f"You have {weeks_left} weeks left")
